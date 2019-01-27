39ae9aa8e8d728d098bde187a43a422e2be2b35d
Edwin Shen <edshen17@avenues.org>
2019-01-26 19:16:40 -0500

    Added help function and added embeds for translate function

--- jishoBot.js
+++ jishoBot.js
@@ -17,7 +17,7 @@ client.on('message', (receivedMessage) => {
     return
   }
 
-  if (receivedMessage.content.startsWith("!")) {
+  if (receivedMessage.content.startsWith("!") || receivedMessage.content.startsWith(".")) {
     processCommand(receivedMessage)
   }
 })
@@ -34,6 +34,10 @@ function processCommand(receivedMessage) {
   } else if (primaryCommand === "define") {
     defineWord(arguments, receivedMessage)
   }
+
+  else if (primaryCommand === "help") {
+    helpMsg(arguments, receivedMessage)
+  }
 }
 //////////////Edwin's code///////////////////////////
 
@@ -41,10 +45,10 @@ function processCommand(receivedMessage) {
 function createSentence(arr, isWord = false) {
   let sentence = ""
   if (!isWord) { //this is really just checking the first arg to see if it's equal to "jp"/"en"
-    arr.shift();
+    arr.shift(); // remove jp/en from the sentence
   }
   arr.forEach((word) => {
-    sentence += " " + word
+    sentence.length === 0 ? sentence += word : sentence += " " + word
   })
   return sentence
 }
@@ -52,46 +56,66 @@ function createSentence(arr, isWord = false) {
 //translates the given message to either jp -> en or en -> jp given inputs and outputs it in Discord
 function translateMsg(arg, msg) {
   let target = arg[0] //target language is the first argument
-  let sentence = ""
-
+  let sentence = ''
+  let footerText = ''
   if (arg[0] === 'jp') { // if user wants to translate to Japanese
     target = 'ja'
     sentence = createSentence(arg)
+    footerText = `English -> Japanese`
   } else if (arg[0] === 'en') { //translate to English
     target = arg[0]
     sentence = createSentence(arg)
+    footerText = 'Japanese -> English'
   } else { //if no input, translate to English by default.
-    //However, this can be made more flexible so you can have it output other languages if the first arg is different
+          //However, this can be made more flexible so you can have it output other languages if the first arg is different
     target = 'en'
     sentence = createSentence(arg, true)
+    footerText = 'Japanese -> English'
   }
 
   //Use Google's API
   translate
     .translate(sentence, target)
     .then(results => {
-      const translation = results[0];
-      msg.reply(translation)
+      const translation = results[0]
+      msg.channel.send({
+      "embed": {
+        "color": 4886754,
+        "footer": {
+          "text": footerText
+        },
+
+        "fields": [{
+          "name": "Translation:",
+          "value": translation
+        }]
+      }
+    })
     })
     .catch(err => {
-      console.error('ERROR:', err);
+      console.error('ERROR:', err)
     });
 }
 
-// Embeds given data and makes it look nice in Discord
-function Embed(msg, word, encoded, json) {
+// Embeds given data from jisho.org and makes it look nice in Discord
+function EmbedJisho(msg, word, encoded, json) {
   let definitions = ''
   let tags = ''
+  const wordTag = json.data[0].tags
+  const isCommon = json.data[0].is_common
+  const jpWord = json.data[0].japanese[0].word
+  const jpReading = json.data[0].japanese[0].reading
+
   json.data[0].senses.forEach((word, index) => { //gets all definitions of the first word and stores them
     definitions += `\r ${index + 1}.  ${word.english_definitions[0]}`
   })
 
-  if (json.data[0].tags.length === 1 && json.data[0].is_common) { //if has Wanikani tag and is a common tag
-    tags += `${json.data[0].tags[0]}, common word`
-  } else if (json.data[0].is_common) { //just a common tag
+  if (wordTag.length === 1 && isCommon) { //if has Wanikani tag and is a common tag
+    tags += `${wordTag[0]}, common word`
+  } else if (isCommon) { //just a common tag
     tags += `common word`
-  } else if (json.data[0].tags.length > 0) { //just a Wanikani tag
-    tags += `${json.data[0].tags[0]}`
+  } else if (wordTag.length > 0) { //just a Wanikani tag
+    tags += `${wordTag[0]}`
   } else {
     tags += 'No tags'
   }
@@ -99,14 +123,14 @@ function Embed(msg, word, encoded, json) {
   //Embeds the data we get from Jisho
   msg.channel.send({
       "embed": {
-        "title": `Link to${word} on Jisho.org`,
+        "title": `Link to ${word} on Jisho.org`,
         "url": `https://jisho.org/search/${encoded}`,
         "color": 2273161,
         "fields": [{
             "name": "Reading(s):",
-            "value": json.data[0].japanese[0].word //if the word is a kanji (not a hiragana/katakana word)
-              ? `${json.data[0].japanese[0].word} (${json.data[0].japanese[0].reading})` //output kanji and reading
-              : `${json.data[0].japanese[0].reading}` //output only the reading
+            "value": jpWord //if the word is a kanji (not a hiragana/katakana word)
+              ? `${jpWord} (${jpReading})` //output kanji and reading
+              : `${jpReading}` //else, output only the reading
           },
           {
             "name": "Definition(s):",
@@ -118,8 +142,7 @@ function Embed(msg, word, encoded, json) {
           }
         ]
       }
-    }
-  );
+    })
 }
 
 //Searches English or Japanese word on Jisho.org
@@ -134,13 +157,71 @@ function defineWord(arg, msg) {
       } else if (json.data.length === 0) {
         msg.reply('Please make sure you entered a valid word!')
       } else {
-        Embed(msg, word, encoded, json);
+        EmbedJisho(msg, word, encoded, json);
       }
     });
   } catch {
     msg.reply('There was an error! The Jisho server may be down, or your link to the dictionary for your BOT is incorect')
   }
 }
+
+//Informs user how to use the BOT's commands
+function helpMsg(arg, msg) {
+  if (arg[0] === 'translate') {
+    msg.channel.send({
+      "embed": {
+        "color": 13632027,
+        "fields": [{
+          "name": "!translate",
+          "value": `!translate en <Japanese sentence> will output an English translation of the sentence given.
+        \r Similarly, !translate jp <English sentence> will output an Japanese translation of the sentence given.
+        \r Example: \r !translate en こんにちは (outputs Hello) \r !translate jp Hello (outputs こんにちは)
+        `
+        }]
+      }
+    })
+  }
+  else if (arg[0] === 'define') {
+    msg.channel.send({
+      "embed": {
+        "color": 13632027,
+        "fields": [{
+          "name": "!define <kana/kanji>",
+          "value": `Outputs the first item listed in Jisho of the inputted word.
+          Please note that sometimes English words can be accidentally turned into romaji due to how Jisho.org works
+          (eg: inputting 'go' can result in 五)
+        \r Example:
+        \r!define こんにちは (outputs the reading, definition, and tags of こんにちは)`
+        }]
+      }
+    })
+  }
+
+  else {
+    msg.channel.send({
+      "embed": {
+        "color": 13632027,
+        "fields": [{
+          "name": "!translate",
+          "value": `!translate en <Japanese sentence> will output an English translation of the sentence given.
+        \r Similarly, !translate jp <English sentence> will output an Japanese translation of the sentence given.
+        \r Example: \r !translate en こんにちは (outputs Hello) \r !translate jp Hello (outputs こんにちは)
+        `
+      },
+        {
+          "name": "!define <kana/kanji>",
+          "value": `Outputs the first item listed in Jisho of the inputted word.
+          Please note that sometimes English words can be accidentally turned into romaji due to how Jisho.org works
+          (eg: inputting 'go' can result in 五)
+        \r Example:
+        \r !define こんにちは (outputs the reading, definition, and tags of こんにちは)`
+        }
+    ]
+      }
+    })
+  }
+}
+
 
 
 
